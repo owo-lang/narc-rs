4dc952e19bca77467dd14373b6dd51953e216b76
ice1000 <ice1000kotlin@foxmail.com>
2019-09-14 19:11:42 -0400

    [ decl ] Fulfil declarations

--- src/syntax/abs/decl.rs
+++ src/syntax/abs/decl.rs
@@ -1,28 +1,70 @@
 use voile_util::level::Level;
-use voile_util::loc::{Ident, Labelled, Loc};
+use voile_util::loc::{Ident, Labelled, Loc, ToLoc};
 
 use super::{Abs, AbsTele};
 use crate::syntax::abs::AbsPat;
 
 /// Declaration.
-/// https://hackage.haskell.org/package/Agda-2.6.0.1/docs/Agda-Syntax-Abstract.html#t:Declaration
+/// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/Agda-Syntax-Abstract.html#t:Declaration).
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum AbsDecl {
-    /// Datatypes. Location, the name, the constructors, and the level.
-    Data(Loc, Ident, Level),
-    /// Datatype constructors. Location, the name, the parameters,
-    /// the corresponding datatype's name, the arguments applied to the datatype.
-    Cons(Loc, Ident, AbsTele, Ident, AbsTele),
-    /// Coinductive record projections. Location, the name, the parameters,
-    /// the corresponding coinductive record's name, the arguments applied to the record.
-    Proj(Loc, Ident, AbsTele, Ident, AbsTele),
+    /// Datatypes.
+    Data {
+        source: Loc,
+        name: Ident,
+        level: Level,
+    },
+    /// Datatype constructors.
+    Cons {
+        source: Loc,
+        name: Ident,
+        params: AbsTele,
+        /// Corresponding datatype's name.
+        data_name: Ident,
+        /// Arguments applied to the datatype.
+        data_tele: AbsTele,
+    },
+    /// Coinductive record projections.
+    Proj {
+        source: Loc,
+        name: Ident,
+        ty: Abs,
+        /// Corresponding coinductive record's name
+        codata_name: Ident,
+        /// Arguments applied to the record.
+        codata_tele: AbsTele,
+    },
     /// Function signature definition.
-    Defn(Loc, Ident, Abs),
-    /// Pattern matching clause. The location, the name, the patterns and the body.
-    Clause(Loc, Ident, Vec<AbsPat>, Abs),
-    /// Coinductive records. Location,
-    /// the self reference, the name, the fields and the level.
-    Codata(Loc, Ident, Ident, Vec<Field>, Level),
+    Defn { source: Loc, name: Ident, ty: Abs },
+    /// Pattern matching clause.
+    Clause {
+        source: Loc,
+        name: Ident,
+        patterns: Vec<AbsPat>,
+        body: Abs,
+    },
+    /// Coinductive records.
+    Codata {
+        source: Loc,
+        self_ref: Ident,
+        name: Ident,
+        fields: Vec<Field>,
+        level: Level,
+    },
+}
+
+impl ToLoc for AbsDecl {
+    fn loc(&self) -> Loc {
+        use AbsDecl::*;
+        match self {
+            Data { source, .. }
+            | Cons { source, .. }
+            | Proj { source, .. }
+            | Defn { source, .. }
+            | Clause { source, .. }
+            | Codata { source, .. } => *source,
+        }
+    }
 }
 
 /// Constructors.
--- src/syntax/core/decl.rs
+++ src/syntax/core/decl.rs
@@ -1,25 +1,43 @@
 use voile_util::level::Level;
 use voile_util::loc::Labelled;
+use voile_util::uid::GI;
 
 use super::{Tele, Term};
 
 /// Declaration.
-/// https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Monad.Base.html#Function
+/// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Monad.Base.html#Function).
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Decl {
-    /// Datatypes. The parameters, the constructors, and the level.
-    Data(Tele, Vec<Constructor>, Level),
+    /// Datatypes.
+    Data {
+        name: String,
+        params: Tele,
+        /// References to its constructors.
+        conses: Vec<GI>,
+        level: Level,
+    },
     /// Coinductive records.
-    /// The self reference, the name, the fields and the level.
-    Codata(String, String, Vec<Field>, Level),
+    Codata {
+        self_ref: String,
+        name: String,
+        /// References to its projections (fields).
+        fields: Vec<GI>,
+        level: Level,
+    },
+    Cons {
+        name: String,
+        params: Tele,
+    },
+    /// Function definitions.
+    Func {
+        name: String,
+        clauses: Vec<Clause>,
+    },
 }
 
-/// Constructors.
-/// This definition follows the paper version instead
-/// of the one in Agda's implementation.
-pub type Constructor = Labelled<Tele>;
-
-/// Fields.
-/// This definition follows the paper version instead
-/// of the one in Agda's implementation.
-pub type Field = Labelled<Term>;
+/// Function clauses.
+/// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.Syntax.Internal.html#Clause).
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Clause {
+    TODO: (),
+}
