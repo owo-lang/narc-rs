851ae85edaa78e09010eb014b9eda6eeb91174a6
ice1000 <ice1000kotlin@foxmail.com>
2019-09-18 21:31:37 -0400

    [ eval ] No message

--- src/check/eval.rs
+++ src/check/eval.rs
@@ -7,6 +7,11 @@ pub fn eval(tcs: TCS, abs: Abs) -> (TermInfo, TCS) {
     use Abs::*;
     match abs {
         Type(ident, level) => (Term::universe(level).into_info(ident.loc), tcs),
+        App(loc, f, a) => {
+            let (f, tcs) = eval(tcs, *f);
+            let (a, tcs) = eval(tcs, *a);
+            unimplemented!()
+        }
         _ => unimplemented!(),
     }
 }
