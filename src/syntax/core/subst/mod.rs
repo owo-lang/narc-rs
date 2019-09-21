cfe6640f032954ac7167b28e2d4bab68321c6ab9
ice1000 <ice1000kotlin@foxmail.com>
2019-09-21 18:32:54 -0400

    [ subst ] Fully implement `subst`

--- src/syntax/core/subst/mod.rs
+++ src/syntax/core/subst/mod.rs
@@ -1,12 +1,12 @@
 use std::rc::Rc;
 
+use either::Either;
 use voile_util::uid::DBI;
 
 use super::{Pat, Term};
 
 pub use self::apply::*;
 pub use self::redex::*;
-use std::ops::Deref;
 
 /// Substitution type.
 /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.Syntax.Internal.html#Substitution%27).
@@ -30,12 +30,19 @@ pub enum PrimSubst<T> {
     /// {\Gamma \vdash \text{Succ} \rho : \Delta, A}
     /// $$
     Succ(Rc<Self>),
-    /// Weakning substitution, lifts to an extended context.
+    /// Weakening substitution, lifts to an extended context.
     /// $$
     /// \cfrac{\Gamma \vdash \rho : \Delta}
-    /// {\Gamma, \Phi \vdash \text{Weak} \mid \Phi \mid \rho : \Delta}
+    /// {\Gamma, \Psi \vdash \text{Weak}_\Psi \rho : \Delta}
     /// $$
     Weak(usize, Rc<Self>),
+    /// Lifting substitution. Use this to go under a binder.
+    /// $\text{Lift}\_1 \rho := \text{Cons}(\texttt{Term::form\\\_dbi(0)}, \text{Weak}\_1 \rho)$.
+    /// $$
+    /// \cfrac{\Gamma \vdash \rho : \Delta}
+    /// {\Gamma, \Psi \rho \vdash \text{Lift}_\Psi \rho : \Delta, \Psi}
+    /// $$
+    Lift(usize, Rc<Self>),
 }
 
 impl<T> Default for PrimSubst<T> {
@@ -47,26 +54,42 @@ impl<T> Default for PrimSubst<T> {
 impl PrimSubst<Term> {
     /// If lookup failed, return the DBI.
     /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#lookupS).
-    pub fn lookup_impl(&self, dbi: DBI) -> Result<&Term, Term> {
+    pub fn lookup_impl(&self, dbi: DBI) -> Either<&Term, Term> {
+        use Either::*;
         use PrimSubst::*;
         match self {
-            IdS => Err(Term::from_dbi(dbi)),
+            IdS => Right(Term::from_dbi(dbi)),
             Cons(o, rest) => match dbi.nat() {
-                None => Ok(o),
+                None => Left(o),
                 Some(dbi) => rest.lookup_impl(dbi),
             },
             Succ(rest) => rest.lookup_impl(dbi.pred()),
             Weak(i, rest) => match &**rest {
-                IdS => Err(Term::from_dbi(dbi + *i)),
-                rho => Err(rho.lookup(DBI(*i)).reduce_dbi(&*Self::raise(*i))),
+                IdS => Right(Term::from_dbi(dbi + *i)),
+                rho => Right(rho.lookup(DBI(*i)).reduce_dbi(&*Self::raise(*i))),
             },
+            Lift(n, rest) => {
+                if dbi < DBI(*n) {
+                    Right(Term::from_dbi(dbi))
+                } else {
+                    Right(Self::raise_term(*n, rest.lookup(DBI(dbi.0 - *n))))
+                }
+            }
         }
     }
 
     pub fn lookup(&self, dbi: DBI) -> Term {
-        self.lookup_impl(dbi)
-            .map(Clone::clone)
-            .unwrap_or_else(|e| e)
+        self.lookup_impl(dbi).map_left(Clone::clone).into_inner()
+    }
+
+    /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#raise).
+    pub fn raise_term(k: usize, term: Term) -> Term {
+        Self::raise_from(0, k, term)
+    }
+
+    /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#raiseFrom).
+    pub fn raise_from(n: usize, k: usize, term: Term) -> Term {
+        term.reduce_dbi(&Self::lift(Self::raise(k), n))
     }
 }
 
@@ -74,6 +97,20 @@ impl<T> PrimSubst<T> {
     /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#raiseS).
     pub fn raise(by: usize) -> Rc<Self> {
         Self::weaken(Default::default(), by)
+    }
+
+    /// Lift a substitution under k binders.
+    /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#liftS).
+    pub fn lift(me: Rc<Self>, by: usize) -> Rc<Self> {
+        use PrimSubst::*;
+        match (by, me) {
+            (0, rho) => rho,
+            (k, rho) => match &*rho {
+                IdS => Default::default(),
+                Lift(n, rho) => Rc::new(Lift(n + k, rho.clone())),
+                _ => Rc::new(Lift(k, rho)),
+            },
+        }
     }
 
     /// [Agda](https://hackage.haskell.org/package/Agda-2.6.0.1/docs/src/Agda.TypeChecking.Substitute.Class.html#wkS).
