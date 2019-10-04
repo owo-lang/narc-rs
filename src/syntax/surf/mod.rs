2bb6c2f0e43a285d27cd221ca9b0c6a77dfe5178
ice1000 <ice1000kotlin@foxmail.com>
2019-10-04 12:32:55 -0400

    [ surf ] Decl parser

--- src/syntax/surf/mod.rs
+++ src/syntax/surf/mod.rs
@@ -1,6 +1,7 @@
 use pest::Parser;
 use pest_derive::Parser;
 
+use voile_util::loc::Ident;
 use voile_util::pest_util::end_of_rule;
 
 #[derive(Parser)]
@@ -8,3 +9,15 @@ use voile_util::pest_util::end_of_rule;
 struct NarcParser;
 
 tik_tok!();
+
+#[inline]
+fn next_ident(inner: &mut Tik) -> Ident {
+    next_rule!(inner, ident)
+}
+
+fn ident(rule: Tok) -> Ident {
+    Ident {
+        text: rule.as_str().to_owned(),
+        loc: From::from(rule.as_span()),
+    }
+}
