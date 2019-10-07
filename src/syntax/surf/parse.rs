555f49f2c6a2fa9d6e73446ff49cdecd3cd681a2
ice1000 <ice1000kotlin@foxmail.com>
2019-10-07 00:00:08 -0400

    [ #31 ] Decl parsing initial

--- src/syntax/surf/parse.rs
+++ src/syntax/surf/parse.rs
@@ -1,6 +1,7 @@
 use pest::Parser;
 use pest_derive::Parser;
 
+use crate::syntax::surf::{Expr, ExprDecl};
 use voile_util::loc::Ident;
 use voile_util::pest_util::end_of_rule;
 
@@ -9,6 +10,34 @@ use voile_util::pest_util::end_of_rule;
 struct NarcParser;
 
 tik_tok!();
+
+define_parse_str!(parse_str, NarcParser, file, decls, Vec<ExprDecl>);
+
+fn decls(the_rule: Tok) -> Vec<ExprDecl> {
+    the_rule.into_inner().into_iter().map(decl).collect()
+}
+
+fn decl(rules: Tok) -> ExprDecl {
+    let mut inner: Tik = rules.into_inner();
+    let the_rule: Tok = inner.next().unwrap();
+    match the_rule.as_rule() {
+        Rule::definition => definition(the_rule),
+        Rule::clause => unimplemented!(),
+        _ => unreachable!(),
+    }
+}
+
+fn definition(rules: Tok) -> ExprDecl {
+    let mut inner: Tik = rules.into_inner();
+    let ident = next_ident(&mut inner);
+    let expr = next_rule!(inner, expr);
+    end_of_rule(&mut inner);
+    ExprDecl::Defn(ident, expr)
+}
+
+fn expr(rules: Tok) -> Expr {
+    unimplemented!()
+}
 
 #[inline]
 fn next_ident(inner: &mut Tik) -> Ident {
