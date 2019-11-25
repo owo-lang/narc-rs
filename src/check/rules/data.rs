ea5050b071a4b2f626b867ca4d2078fd83629408
ice1000 <ice1000kotlin@foxmail.com>
2019-11-25 02:20:27 -0500

    [ #17 ] Check conses

--- src/check/rules/data.rs
+++ src/check/rules/data.rs
@@ -1,10 +1,10 @@
 use crate::check::monad::{TCM, TCS};
 use crate::check::rules::check;
-use crate::syntax::abs::{AbsDataInfo, AbsTele};
-use crate::syntax::core::{DataInfo, Term, TYPE_OMEGA};
+use crate::syntax::abs::{AbsConsInfo, AbsDataInfo, AbsTele};
+use crate::syntax::core::{ConsInfo, DataInfo, Term, TYPE_OMEGA};
 
 /// The checked tele is put into the returned `tcs.gamma`.
-pub fn check_tele(mut tcs: TCS, tele: AbsTele) -> TCM<TCS> {
+pub fn check_tele(mut tcs: TCS, tele: AbsTele) -> TCM {
     for bind in tele {
         let (checked, new_tcs) = check(tcs, &bind.ty, &TYPE_OMEGA)?;
         tcs = new_tcs;
@@ -14,10 +14,47 @@ pub fn check_tele(mut tcs: TCS, tele: AbsTele) -> TCM<TCS> {
     Ok(tcs)
 }
 
-pub fn check_data(tcs: TCS, data: AbsDataInfo) -> TCM<(TCS, DataInfo)> {
-    let t = Term::universe(data.level);
-    let tcs = check_tele(tcs, data.tele)?;
+pub fn check_cons(tcs: TCS, cons: AbsConsInfo) -> TCM<(TCS, ConsInfo)> {
     let param_len = tcs.gamma.len();
-    // TODO: check conses
-    unimplemented!()
+    let mut tcs = check_tele(tcs, cons.tele)?;
+    let info = ConsInfo {
+        loc: cons.source,
+        name: cons.name.text,
+        params: tcs.gamma.split_off(param_len),
+        data: cons.data_ix,
+        // Inductive!
+        fields: None,
+    };
+    Ok((tcs, info))
+}
+
+pub type DataTCS = (TCS, DataInfo, Vec<ConsInfo>);
+
+pub fn check_data(tcs: TCS, data: AbsDataInfo, conses: Vec<AbsConsInfo>) -> TCM<DataTCS> {
+    let t = Term::universe(data.level);
+    let mut tcs = check_tele(tcs, data.tele)?;
+    let param_len = tcs.gamma.len();
+    // For debugging only.
+    let mut data_ix = None;
+    let mut cons_collect = Vec::with_capacity(conses.len());
+    for cons in conses {
+        let (new_tcs, cons) = check_cons(tcs, cons)?;
+        tcs = new_tcs;
+        match data_ix {
+            None => data_ix = Some(cons.data),
+            Some(ix) => debug_assert_eq!(ix, cons.data),
+        }
+        debug_assert_eq!(param_len, tcs.gamma.len());
+        cons_collect.push(cons);
+    }
+    let mut params = vec![];
+    std::mem::swap(&mut params, &mut tcs.gamma);
+    let info = DataInfo {
+        params,
+        loc: data.source,
+        name: data.name.text,
+        level: data.level,
+        conses: data.conses,
+    };
+    Ok((tcs, info, cons_collect))
 }
