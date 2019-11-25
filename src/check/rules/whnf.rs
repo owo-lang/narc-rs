843dd11ac725003a5b706fa403d42e46f663bf0c
ice1000 <ice1000kotlin@foxmail.com>
2019-11-24 23:07:28 -0500

    [ whnf ] Whnf constructor applications

--- src/check/rules/whnf.rs
+++ src/check/rules/whnf.rs
@@ -1,5 +1,8 @@
+use voile_util::loc::Ident;
+
 use crate::check::monad::{ValTCM, TCE, TCM, TCS};
-use crate::syntax::core::{Decl, Elim, Term};
+use crate::syntax::common::Ductive;
+use crate::syntax::core::{ConHead, Decl, Elim, Term};
 
 pub fn normalize(tcs: TCS, term: Term) -> ValTCM {
     match term {
@@ -7,7 +10,25 @@ pub fn normalize(tcs: TCS, term: Term) -> ValTCM {
         Term::Redex(def, elims) => match tcs.def(def) {
             Decl::Data { .. } => normalize(tcs, Term::inductive(def, elims_to_terms(elims)?)),
             Decl::Codata { .. } => normalize(tcs, Term::coinductive(def, elims_to_terms(elims)?)),
-            Decl::Cons { .. } => unimplemented!(),
+            Decl::Cons {
+                loc,
+                name,
+                data,
+                fields,
+                ..
+            } => {
+                let (ductive, fields) = match fields {
+                    Some(fields) => (Ductive::In, fields.clone()),
+                    None => (Ductive::Coin, vec![]),
+                };
+                let ident = Ident {
+                    loc: *loc,
+                    text: name.clone(),
+                };
+                // How can I find the real name ident?
+                let head = ConHead::new(ident, *data, ductive, fields);
+                normalize(tcs, Term::cons(head, elims_to_terms(elims)?))
+            }
             Decl::Proj { .. } => unimplemented!(),
             // TODO: build up a substitution and unfold the declaration.
             Decl::Func { .. } => unimplemented!(),
