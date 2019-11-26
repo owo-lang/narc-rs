b77eaf8af68a5f0289f538e9666864b17fe68dec
ice1000 <ice1000kotlin@foxmail.com>
2019-11-26 09:27:48 -0500

    [ desugar ] Test desugar data declarations

--- src/syntax/abs/desugar/tests.rs
+++ src/syntax/abs/desugar/tests.rs
@@ -2,17 +2,25 @@ use voile_util::meta::MI;
 use voile_util::uid::{GI, UID};
 
 use crate::syntax::abs::desugar::desugar_main;
-use crate::syntax::abs::{Abs, AbsClause, AbsCopat, AbsDecl};
+use crate::syntax::abs::*;
 use crate::syntax::pat::{Copat, Pat};
 use crate::syntax::surf::parse_str;
 
-fn expect_clause(def: AbsDecl) -> AbsClause {
-    if let AbsDecl::Clause(c) = def {
-        c
-    } else {
-        panic!("Test failed")
-    }
+macro_rules! make_expect_decl {
+    ($name:ident, $pat:ident, $ret:ty) => {
+        fn $name(def: AbsDecl) -> $ret {
+            if let AbsDecl::$pat(c) = def {
+                c
+            } else {
+                panic!("Test failed")
+            }
+        }
+    };
 }
+
+make_expect_decl!(expect_clause, Clause, AbsClause);
+make_expect_decl!(expect_data, Data, AbsDataInfo);
+make_expect_decl!(expect_constructor, Cons, AbsConsInfo);
 
 fn expect_var_expr(p: Abs) -> UID {
     if let Abs::Var(_, uid) = p {
@@ -31,6 +39,27 @@ fn expect_app_var_pat(p: AbsCopat) -> UID {
 }
 
 #[test]
+fn simple_data_and_constructor_desugar() {
+    let code = "\
+    data test {
+      constructor test-cons;
+    };
+    ";
+    let mut state = desugar_main(parse_str(code).unwrap()).unwrap();
+    println!("{:#?}", state);
+    assert_eq!(state.decls.len(), 2);
+    assert!(state.local.is_empty());
+    let cons: AbsConsInfo = expect_constructor(state.decls.remove(1));
+    let data: AbsDataInfo = expect_data(state.decls.remove(0));
+    assert!(data.tele.is_empty());
+    assert!(cons.tele.is_empty());
+    assert_eq!(cons.name.text, "test-cons");
+    assert_eq!(data.name.text, "test");
+    assert_eq!(data.conses, vec![GI(1)]);
+    assert_eq!(cons.data_ix, GI(0));
+}
+
+#[test]
 fn simple_pattern_definition_desugar() {
     let code = "\
     definition test : (a : Type) -> Type a;
@@ -39,6 +68,7 @@ fn simple_pattern_definition_desugar() {
     let mut state = desugar_main(parse_str(code).unwrap()).unwrap();
     println!("{:#?}", state);
     assert!(state.local.is_empty());
+    assert_eq!(state.decls.len(), 2);
     let mut c = expect_clause(state.decls.remove(1));
     assert_eq!(c.patterns.len(), 1);
     assert_eq!(
