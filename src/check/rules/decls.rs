0bfe929ea87b327b4b71b2d8ebce45c3a1eb155b
ice1000 <ice1000kotlin@foxmail.com>
2019-12-04 17:54:47 -0500

    [ decls ] Integrate `clause`

--- src/check/rules/decls.rs
+++ src/check/rules/decls.rs
@@ -1,8 +1,13 @@
+use std::hint::unreachable_unchecked;
+
 use voile_util::uid::GI;
 
 use crate::check::monad::{TCM, TCS};
+use crate::check::rules::clause::clause;
 use crate::check::rules::data::check_data;
+use crate::check::rules::term::check;
 use crate::syntax::abs::AbsDecl;
+use crate::syntax::core::{Decl, FuncInfo, TYPE_OMEGA};
 
 use super::ERROR_TAKE;
 
@@ -27,7 +32,31 @@ pub fn check_decls(mut tcs: TCS, decls: Vec<AbsDecl>) -> TCM {
                 tcs = check_data(tcs, i, cs)?;
             }
             AbsDecl::Cons(_) => unreachable!(ERROR_TAKE),
-            AbsDecl::Clause(_) => unreachable!(ERROR_TAKE),
+            AbsDecl::Defn(i) => {
+                let (ty, new_tcs) = check(tcs, &i.ty, &TYPE_OMEGA)?;
+                tcs = new_tcs;
+                let func = FuncInfo {
+                    loc: i.source,
+                    name: i.name,
+                    signature: ty.ast,
+                    clauses: Vec::with_capacity(2),
+                };
+                tcs.sigma.push(Decl::Func(func));
+            }
+            AbsDecl::Clause(i) => {
+                let def_ix = i.definition;
+                let signature = match tcs.def(def_ix) {
+                    Decl::Func(f) => f.signature.clone(),
+                    _ => unreachable!(),
+                };
+                let (cls, new_tcs) = clause(tcs, i, signature)?;
+                tcs = new_tcs;
+                match tcs.mut_def(def_ix) {
+                    Decl::Func(f) => f.clauses.push(cls),
+                    _ => unsafe { unreachable_unchecked() },
+                };
+                tcs.sigma.push(Decl::ClausePlaceholder);
+            }
             _ => unimplemented!(),
         }
     }
