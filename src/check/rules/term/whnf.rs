c32c167a28371fa8a4712ef4d4bfbd7692000bbd
ice1000 <ice1000kotlin@foxmail.com>
2019-12-13 18:41:53 -0500

    [ whnf ] Start working on unfold defs

--- src/check/rules/term/whnf.rs
+++ src/check/rules/term/whnf.rs
@@ -1,6 +1,6 @@
 use crate::check::monad::{ValTCM, TCE, TCM, TCS};
 use crate::syntax::common::Ductive;
-use crate::syntax::core::{ConHead, Decl, Elim, Term, Val};
+use crate::syntax::core::{ConHead, Decl, Elim, FuncInfo, Term, Val};
 
 pub fn simplify(tcs: TCS, term: Term) -> ValTCM {
     match term {
@@ -17,17 +17,21 @@ pub fn simplify(tcs: TCS, term: Term) -> ValTCM {
                 Ok((Val::Cons(head, elims_to_terms(elims)?), tcs))
             }
             Decl::Proj { .. } => unimplemented!(),
-            // TODO: build up a substitution and unfold the declaration.
-            Decl::Func { .. } => unimplemented!(),
+            Decl::Func(func) => unfold_func(func.clone(), tcs, elims),
             Decl::ClausePlaceholder => unreachable!(),
         },
     }
 }
 
+// TODO: build up a substitution and unfold the declaration.
+fn unfold_func(func: FuncInfo, tcs: TCS, elims: Vec<Elim>) -> ValTCM {
+    unimplemented!()
+}
+
 fn elims_to_terms(elims: Vec<Elim>) -> TCM<Vec<Term>> {
     elims
         .into_iter()
-        .map(|elim| elim.try_into_app())
+        .map(Elim::try_into_app)
         .collect::<Result<_, String>>()
         .map_err(TCE::NotTerm)
 }
