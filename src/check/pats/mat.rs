8adbf80c5d73aee7e83e587be77a2ddbfef7e784
ice1000 <ice1000kotlin@foxmail.com>
2019-12-14 15:23:55 -0500

    [ mat.rs ] Many instances for `Match`

--- src/check/pats/mat.rs
+++ src/check/pats/mat.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::Add;
 
 use voile_util::uid::DBI;
 
@@ -31,8 +32,46 @@ impl Into<bool> for Simpl {
     }
 }
 
+impl Default for Simpl {
+    fn default() -> Self {
+        Simpl::No
+    }
+}
+
+impl Add for Simpl {
+    type Output = Simpl;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match self {
+            Simpl::Yes => Simpl::Yes,
+            Simpl::No => rhs,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 enum Match {
     Yes(Simpl, HashMap<DBI, Term>),
     No,
+}
+
+impl Default for Match {
+    fn default() -> Self {
+        Match::Yes(Default::default(), Default::default())
+    }
+}
+
+impl Add for Match {
+    type Output = Match;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (Match::No, o) => o,
+            (o, Match::No) => o,
+            (Match::Yes(s0, mut m0), Match::Yes(s1, m1)) => {
+                m0.extend(m1.into_iter());
+                Match::Yes(s0 + s1, m0)
+            }
+        }
+    }
 }
